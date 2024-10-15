f8e7e586c801428a929e9a64dd2345f3a3ece433
magisterblaman <hannes.blaman@lbs.se>
2024-10-15 11:40:52 +0200

    Adder

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import flet
 
 def main(page: flet.Page):
-    page.add(flet.Text("Hello world"))
+
+    number_text = flet.Text("0")
+
+    def minus_click(e):
+        prev_value = int(number_text.value)
+        new_value = prev_value - 1
+        number_text.value = str(new_value)
+        number_text.update()
+
+    def plus_click(e):
+        prev_value = int(number_text.value)
+        new_value = prev_value + 1
+        number_text.value = str(new_value)
+        number_text.update()
+
+    row = flet.Row(
+        controls=[
+            flet.TextButton("-", on_click=minus_click),
+            number_text,
+            flet.TextButton("+", on_click=plus_click)
+        ]
+    )
+    page.add(row)
+
 
 flet.app(main)
